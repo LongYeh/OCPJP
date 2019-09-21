df79122829c0cde9f6854be3d3ecb14b2ec503bf
Mr.YEH <zxc09267520@gmail.com>
2019-09-21 22:11:34 +0800

    add new Question

---
+++ src/main/java/com/test/Question110.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Question110 {
+    public static void main(String[] args) {
+        float a=100;
+        int [] stack={10,20,30};
+        int size=3;
+        int idx=0;
+        while(idx<size-1){
+            idx++;
+        }
+
+        System.out.println("The Top elements: "+stack[idx]);
+    }
+}
